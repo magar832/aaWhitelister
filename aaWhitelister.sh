9605814337c232b484bd6e30e1b335515e0478e8
Matthew Garcia <magar832@gmail.com>
2022-01-04 04:54:59 -0600

    Added the script

---
+++ aaWhitelister.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+dlpath=$HOME
+
+## Firstrun check
+FILE=$dlpath/aaWhitelist_formatted.txt
+if [ -f "$FILE" ]; then
+	echo "$FILE exists."
+else
+	echo "Creating non-empty whitelist file..."
+	echo "example.com" >> $dlpath/aaWhitelist_formatted.txt
+fi
+
+## Variable holds finalized file contents
+whitelist=$(cat $dlpath/aaWhitelist_formatted.txt)
+
+## Delete current whitelist entries using previously saved whitelist
+echo "Removing previous AAdvantage whitelist entries"
+pihole -w -d $whitelist
+
+## Download whitelist, format it
+echo "Downloading new whitelist..."
+curl -L https://www.aadvantageeshopping.com/adBlockWhitelist.php -o $dlpath/aaWhitelist.txt 
+sed -i '1,4d' $dlpath/aaWhitelist.txt ## Remove header
+sed -i 's/^@@||//g' $dlpath/aaWhitelist.txt ## remove the @@|| in each line
+tr '\n' ' ' < $dlpath/aaWhitelist.txt > $dlpath/aaWhitelist_formatted.txt ## Replace newline to whitespace
+
+## Upload whitelist to Gravity
+whitelist=$(cat $dlpath/aaWhitelist_formatted.txt)
+echo "These domains will be added: " $whitelist
+pihole -w --comment "AAdvantage eShopping" $whitelist 
